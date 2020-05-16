24c7cc63fb0cf5da7a44f982a0f2567d7d74158e
Diannedejong <150512@csgaugustinus.nl>
2020-05-16 13:35:06 +0000

    cdvd

--- 1_1.php
+++ 1_1.php
@@ -1,20 +1,11 @@
-
-
-<?PHP
-$een= "ab";
-echo "$een"
-
-?>
-
-<!DOCTYPE html>
 <html>
 <head>
-
 </head>
 <body>
-<h1>hoe the fuck werkt dit</h1>
+<?PHP
+echo'hello wordl';
+?>
+
+
 </body>
-
-
-
 </html>
